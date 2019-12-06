c30317fac9c8fd05b55d0d6e1889649eff4219f9
bekwdev <bekwdev@gmail.com>
2019-12-06 11:28:07 -0500

    Added IndexScreen and BlogContext as a  provider and managed connection between them ,Added FlatList which contains blogPosts and Button with function addBlogPost to indexScreen

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet, FlatList, Button} from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+  const {data, addBlogPost} = useContext (BlogContext);
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button onPress={addBlogPost} title="ADD" />
+      <FlatList
+        data={data}
+        keyExtractor={data => data.title}
+        renderItem={({item}) => {
+          console.log (item.title);
+          return (
+            <View>
+              <Text>{item.title}</Text>
+
+            </View>
+          );
+        }}
+      />
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create ({});
+
+export default IndexScreen;
